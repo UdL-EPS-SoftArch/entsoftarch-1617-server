21f1e6f289c916d67a2c3a260a8e75b3c5d598de
Roberto García <roberto@rhizomik.net>
2017-02-21 13:00:32 +0100

    Spring project template from Spring Initializr

---
+++ src/main/java/cat/udl/eps/entsoftarch/DemoApplication.java
@@ -0,0 +1,12 @@
+package cat.udl.eps.entsoftarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
