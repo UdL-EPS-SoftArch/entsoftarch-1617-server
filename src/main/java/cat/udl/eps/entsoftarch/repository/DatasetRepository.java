36b0931caa6b2051df0fd78b1bdd97a4427b8690
Roberto García <roberto@rhizomik.net>
2017-02-23 13:24:24 +0100

    Add find by description to dataset repository

--- src/main/java/cat/udl/eps/entsoftarch/repository/DatasetRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/repository/DatasetRepository.java
@@ -2,11 +2,15 @@ package cat.udl.eps.entsoftarch.repository;
 
 import cat.udl.eps.entsoftarch.domain.Dataset;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 /**
  * Created by http://rhizomik.net/~roberto/
  */
 @RepositoryRestResource
 public interface DatasetRepository extends PagingAndSortingRepository<Dataset, Long> {
+    List<Dataset> findByDescription(@Param("description") String description);
 }
