5bf1415745e9d6b2c15e3268ea9643ab9b715a7d
yatan <yatan87@gmail.com>
2017-02-27 16:53:47 +0100

    SearchDatasetByDescription feature

---
+++ src/test/java/cat/udl/eps/entsoftarch/steps/SearchDatasetByDescriptionStepDefs.java
@@ -0,0 +1,15 @@
+package cat.udl.eps.entsoftarch.steps;
+
+import cucumber.api.PendingException;
+
+/**
+ * Created by yatan on 2/27/17.
+ */
+public class SearchDatasetByDescriptionStepDefs {
+    public SearchDatasetByDescriptionStepDefs() {
+        Then("^Show (\\d+) datasets$", (Integer arg0) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+    }
+}
