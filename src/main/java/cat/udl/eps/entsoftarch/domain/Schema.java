da1ec21bf2bb1993d88e9fac41f2b948507e891a
gerard <gerar.f87@gmail.com>
2017-02-28 15:26:36 +0100

    Create Schema class

---
+++ src/main/java/cat/udl/eps/entsoftarch/domain/Schema.java
@@ -0,0 +1,66 @@
+package cat.udl.eps.entsoftarch.domain;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by gerard on 28/02/17.
+ */
+public class Schema {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    private Dataset dataset;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Dataset getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(Dataset dataset) {
+        this.dataset = dataset;
+    }
+
+    @Override
+    public String toString() {
+        return "Schema{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dataset=" + dataset +
+                '}';
+    }
+}
