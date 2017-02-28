f05b2f521f7ade9bcfdf55d9bc6685810f623638
Roberto García <roberto@rhizomik.net>
2017-02-28 19:06:48 +0100

    Enable authentication using database users

--- src/main/java/cat/udl/eps/entsoftarch/config/AuthenticationConfig.java
+++ src/main/java/cat/udl/eps/entsoftarch/config/AuthenticationConfig.java
@@ -1,5 +1,11 @@
 package cat.udl.eps.entsoftarch.config;
 
+import cat.udl.eps.entsoftarch.domain.DataOwner;
+import cat.udl.eps.entsoftarch.domain.DataUser;
+import cat.udl.eps.entsoftarch.domain.User;
+import cat.udl.eps.entsoftarch.repository.UserRepository;
+import cat.udl.eps.entsoftarch.service.BasicUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,19 +19,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Profile("!Test")
 public class AuthenticationConfig extends GlobalAuthenticationConfigurerAdapter {
 
+    @Autowired BasicUserDetailsService userDetailsService;
+    @Autowired UserRepository userRepository;
+
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+        auth.inMemoryAuthentication()
                 .passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("user")
-                    .password("$2a$10$B1dcscvS/lgiBnGdkhhupew8AhbjqUL7TjdA2ggvxQhs5jN7KVSMC")
-                    .roles("USER").and()
-                .withUser("owner")
-                    .password("$2a$10$B1dcscvS/lgiBnGdkhhupew8AhbjqUL7TjdA2ggvxQhs5jN7KVSMC")
-                    .roles("OWNER").and()
                 .withUser("admin")
                     .password("$2a$10$B1dcscvS/lgiBnGdkhhupew8AhbjqUL7TjdA2ggvxQhs5jN7KVSMC")
                     .roles("ADMIN");
+
+        User owner = new DataOwner();
+        owner.setUsername("owner");
+        owner.setPassword("$2a$10$B1dcscvS/lgiBnGdkhhupew8AhbjqUL7TjdA2ggvxQhs5jN7KVSMC");
+        userRepository.save(owner);
+
+        User user = new DataUser();
+        user.setUsername("user");
+        user.setPassword("$2a$10$B1dcscvS/lgiBnGdkhhupew8AhbjqUL7TjdA2ggvxQhs5jN7KVSMC");
+        userRepository.save(user);
     }
 }
