771e70c7555c57ee7d99af18317bbb3e75aa24f4
yatan <yatan87@gmail.com>
2017-02-28 15:10:48 +0100

    Search by description

--- src/test/java/cat/udl/eps/entsoftarch/steps/SearchDatasetByDescriptionStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/steps/SearchDatasetByDescriptionStepDefs.java
@@ -1,15 +1,37 @@
 package cat.udl.eps.entsoftarch.steps;
 
+import cat.udl.eps.entsoftarch.domain.Dataset;
+import cat.udl.eps.entsoftarch.repository.DatasetRepository;
 import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 /**
  * Created by yatan on 2/27/17.
  */
 public class SearchDatasetByDescriptionStepDefs {
-    public SearchDatasetByDescriptionStepDefs() {
-        Then("^Show (\\d+) datasets$", (Integer arg0) -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
+
+    @Autowired private StepDefs stepDefs;
+    @Autowired private DatasetRepository datasetRepository;
+
+    @When("^I search with a blank description$")
+    public void iSearchWithABlankDescription() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
+
+    @When("^I search with \"([^\"]*)\"$")
+    public void iSearchWith(String description) throws Throwable {
+        Dataset dataset = datasetRepository.findByDescription(description).get(0);
+        throw new PendingException();
+    }
+
+    @Then("^Show (\\d+) datasets$")
+    public void showDatasets(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
 }
