7705a80f71429e45bdc5743ea83c306de397f88a
Pau <pbalaguer19@gmail.com>
2017-03-02 15:25:52 +0100

    Create new step def. #3

--- src/test/java/cat/udl/eps/entsoftarch/steps/UpdateDatasetStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/steps/UpdateDatasetStepDefs.java
@@ -2,13 +2,22 @@ package cat.udl.eps.entsoftarch.steps;
 
 import cat.udl.eps.entsoftarch.domain.Dataset;
 import cat.udl.eps.entsoftarch.repository.DatasetRepository;
+import com.jayway.jsonpath.JsonPath;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
 import static cat.udl.eps.entsoftarch.steps.AuthenticationStepDefs.authenticate;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -16,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
  * Created by pau on 28/2/17.
  */
 public class UpdateDatasetStepDefs {
-    private static final Logger logger = LoggerFactory.getLogger(RegisterDatasetStepdefs.class);
+    private static final Logger logger = LoggerFactory.getLogger(UpdateDatasetStepDefs.class);
 
     @Autowired
     private StepDefs stepDefs;
@@ -36,6 +45,14 @@ public class UpdateDatasetStepDefs {
                         .accept(MediaType.APPLICATION_JSON)
                         .with(authenticate()))
                 .andDo(print());
+    }
+
+    @And("^The new dataset has lastModified field approximately now$")
+    public void theNewDatasetHasLastModifiedFieldApproximatelyNow() throws Throwable {
+        String response = stepDefs.result.andReturn().getResponse().getContentAsString();
+        ZonedDateTime dateTime = ZonedDateTime.parse(JsonPath.read(response, "$.lastModified"));
+        Assert.assertThat(dateTime, greaterThan(ZonedDateTime.now().minus(5, ChronoUnit.SECONDS)));
+        Assert.assertThat(dateTime, lessThan(ZonedDateTime.now()));
     }
 }
 
