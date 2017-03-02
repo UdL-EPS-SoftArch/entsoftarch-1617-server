7c7f20e285dfa0d598f9825a5234c79b896b0db6
gerard <gerar.f87@gmail.com>
2017-03-02 16:19:33 +0100

    Add schemas list to DataOwner

--- src/main/java/cat/udl/eps/entsoftarch/domain/DataOwner.java
+++ src/main/java/cat/udl/eps/entsoftarch/domain/DataOwner.java
@@ -19,6 +19,9 @@ public class DataOwner extends User {
     @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
     private List<Dataset> owns = new ArrayList<>();
 
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+    private List<Schema> owns_schemas = new ArrayList<>();
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_OWNER");
@@ -27,4 +30,12 @@ public class DataOwner extends User {
     public List<Dataset> getOwns() { return owns; }
 
     public void setOwns(List<Dataset> owns) { this.owns = owns; }
+
+    public List<Schema> getOwns_schemas() {
+        return owns_schemas;
+    }
+
+    public void setOwns_schemas(List<Schema> owns_schemas) {
+        this.owns_schemas = owns_schemas;
+    }
 }
