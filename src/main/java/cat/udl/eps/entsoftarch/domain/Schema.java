a5405a0a2a09f9ace77797e51e850c64fca9e2a8
gerard <gerar.f87@gmail.com>
2017-03-02 16:09:17 +0100

    Change schema to lombok data

--- src/main/java/cat/udl/eps/entsoftarch/domain/Schema.java
+++ src/main/java/cat/udl/eps/entsoftarch/domain/Schema.java
@@ -6,11 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
 
 /**
  * Created by gerard on 28/02/17.
  */
 @Entity
+@Data
 public class Schema {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -24,45 +26,4 @@ public class Schema {
 
     private String owner;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    @Override
-    public String toString() {
-        return "Schema{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", owner='" + owner + '\'' +
-                '}';
-    }
 }
