7c5bd7bcea374e5a9e4cb86e9664a204725e0a39
Victor <sarte_789@hotmail.com>
2017-03-03 19:00:37 +0100

    Implemented 2 scenarios and work fine. Created repositories for each licenses

--- src/test/java/cat/udl/eps/entsoftarch/steps/RegisterLicenseStepdefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/steps/RegisterLicenseStepdefs.java
@@ -1,15 +1,12 @@
 package cat.udl.eps.entsoftarch.steps;
 
-import cat.udl.eps.entsoftarch.domain.License;
+import cat.udl.eps.entsoftarch.domain.ClosedLicense;
 import cat.udl.eps.entsoftarch.domain.OpenLicense;
-import cat.udl.eps.entsoftarch.repository.DataOwnerRepository;
-import cat.udl.eps.entsoftarch.repository.LicenseRepository;
-import cucumber.api.PendingException;
+import cat.udl.eps.entsoftarch.repository.ClosedLicenseRepository;
+import cat.udl.eps.entsoftarch.repository.OpenLicenseRepository;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
@@ -23,49 +20,76 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Created by victorserrate on 2/3/17.
+ * Created by Victor on 02/03/2017.
  */
 public class RegisterLicenseStepdefs {
-
-    private static final Logger logger = LoggerFactory.getLogger(RegisterDatasetStepdefs.class);
 
     @Autowired
     private StepDefs stepDefs;
     @Autowired
-    private DataOwnerRepository dataOwnerRepository;
+    private OpenLicenseRepository openLicenseRepository;
     @Autowired
-    private LicenseRepository licenseRepository;
+    private ClosedLicenseRepository closedLicenseRepository;
 
-    @And("^There (?:are|is) (\\d+) licenses? registered$")
-    public void thereAreLicensesRegistered(int count) throws Throwable {
-        stepDefs.result = stepDefs.mockMvc.perform(get("/licenses")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(jsonPath("$._embedded.licenses", hasSize(count)));
-    }
-
-
-
+    //Open License
     @When("^I register a license with text \"([^\"]*)\"$")
-    public void iRegisterALicenseWithText(String text) throws Throwable{
-        License license = new OpenLicense();
+    public void iRegisterAOpenLicenseWithText(String text) throws Throwable {
+        OpenLicense license = new OpenLicense();
         license.setText(text);
+
         String message = stepDefs.mapper.writeValueAsString(license);
 
-        stepDefs.result = stepDefs.mockMvc.perform(
-                post("/licenses")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(message)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(authenticate()))
+        stepDefs.result = stepDefs.mockMvc.perform(post("/openlicenses")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(message)
+                .accept(MediaType.APPLICATION_JSON)
+                .with(authenticate()))
                 .andDo(print());
     }
 
+    @And("^There (?:are|is) (\\d+) open licenses? registered$")
+    public void thereAreOpenLicensesRegistered(int count) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(get("/openlicenses")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.openlicenses", hasSize(count)));
+    }
+
     @Then("^The new license has text \"([^\"]*)\"$")
-    public void theNewLicenseHasText(String text) throws Throwable {
+    public void theNewOpenLicenseHasText(String text) throws Throwable {
         stepDefs.result.andExpect(jsonPath("$.text", is(text)));
     }
+
+    //Closed License
+    @When("^I register a closed license with text \"([^\"]*)\" and price (\\d+)$")
+    public void iRegisterAClosedLicenseWithTextAndPrice(String text, double price) throws Throwable {
+        ClosedLicense license = new ClosedLicense();
+        license.setText(text);
+        license.setPrice(price);
+
+        String message = stepDefs.mapper.writeValueAsString(license);
+
+        stepDefs.result = stepDefs.mockMvc.perform(post("/closedlicenses")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(message)
+                .accept(MediaType.APPLICATION_JSON)
+                .with(authenticate()))
+                .andDo(print());
+    }
+
+    @And("^There (?:are|is) (\\d+) closed licenses? registered$")
+    public void thereIsClosedLicenseRegistered(int count) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(get("/closedlicenses")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.closedlicenses", hasSize(count)));
+    }
+
+    @Then("^The new closed license has text \"([^\"]*)\" and price (\\d+)$")
+    public void theNewClosedLicenseHasTextAndPrice(String text, double price) throws Throwable {
+        stepDefs.result.andExpect(jsonPath("$.text", is(text)));
+        stepDefs.result.andExpect(jsonPath("$.price", is(price)));
+    }
 }
-
-
--- src/test/resources/features/RegisterLicense.feature
+++ src/test/resources/features/RegisterLicense.feature
@@ -3,46 +3,16 @@ Feature: Register License
   As a data owner
   I want to register a license and make it available
 
-Scenario: Register a open license text
-  Given I login as "owner" with password "password"
-  And There are 0 licenses registered
-  When I register a license with text "text"
-  Then The new license has text "text"
-  And There is 1 license registered
+  Scenario: Register a open license with text
+    Given I login as "owner" with password "password"
+    And There are 0 open licenses registered
+    When I register a license with text "text"
+    Then The new license has text "text"
+    And There is 1 open license registered
 
-"""Scenario: Register a closed license text and price
-  Given I login as "owner" with password "password"
-  And There are 0 licenses registered
-  When I register a license with text "text" and price "price"
-  Then The new license has text "text" and price "price"
-  And There is 1 license registered
-
-Scenario: Register a new open license if already registered
-  Given I login as "owner" with password "password"
-  And There is a license with text "my original text" and owner "owner"
-  And There are 1 license registered
-  When I register a license with text "text"
-  Then There are 1 licenses registered
-
-Scenario: Register a new closed license if already registered
-  Given I login as "owner" with password "password"
-  And There is a license with text "my original text" and price "my original price" and owner "owner"
-  And There are 1 license registered
-  When I register a license with text "my own text" and price "my own price"
-  Then There are 1 license registered
-
-Scenario: Register a open license but wrong password
-  Given I login as "owner" with password "wrongpassword"
-  And There are 0 licenses registered
-  When I register a license with text "text"
-  Then The response code is 401
-  And The error message is "Bad credentials"
-  And There are 0 licenses registered
-
-Scenario: Register a closed license but wrong password
-  Given I login as "owner" with password "wrongpassword"
-  And There are 0 licenses registered
-  When I register a license with text "text" and price "price"
-  Then The response code is 401
-  And The error message is "Bad credentials"
-  And There are 0 licenses registered"""
+  Scenario: Register a closed license with text and price
+    Given I login as "owner" with password "password"
+    And There are 0 closed licenses registered
+    When I register a closed license with text "text" and price 10
+    Then The new closed license has text "text" and price 10
+    And There is 1 closed license registered
