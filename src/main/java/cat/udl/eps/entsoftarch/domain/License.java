8c55aaa398619fb056110566c11c911945c3ff0d
Sergi Plaza <plazacagigos@gmail.com>
2017-03-04 12:43:48 +0100

    New scenarios implemented

--- src/main/java/cat/udl/eps/entsoftarch/domain/License.java
+++ src/main/java/cat/udl/eps/entsoftarch/domain/License.java
@@ -1,12 +1,8 @@
 package cat.udl.eps.entsoftarch.domain;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by victorserrate on 2/3/17.
@@ -19,8 +15,11 @@ public abstract class License {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @ManyToOne
+    private DataOwner owner;
+
     @NotBlank
-    public String text;
+    private String text;
 
     public String getText() {
         return text;
@@ -36,6 +35,32 @@ public abstract class License {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public DataOwner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(DataOwner owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        License license = (License) o;
+
+        if (owner != null ? !owner.equals(license.owner) : license.owner != null) return false;
+        return text != null ? text.equals(license.text) : license.text == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = owner != null ? owner.hashCode() : 0;
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        return result;
     }
 
     @Override
