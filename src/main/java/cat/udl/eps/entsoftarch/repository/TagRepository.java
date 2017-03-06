e4de3213ceff52f66008ebccf8977aaa179b427f
joanj94 <joanj_94@hotmail.com>
2017-03-06 15:35:14 +0100

    - Created Tag repository Interface (Issue #18)

---
+++ src/main/java/cat/udl/eps/entsoftarch/repository/TagRepository.java
@@ -0,0 +1,16 @@
+package cat.udl.eps.entsoftarch.repository;
+
+import cat.udl.eps.entsoftarch.domain.Tag;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * Created by UNI on 06/03/2017.
+ */
+@RepositoryRestResource
+public interface TagRepository extends PagingAndSortingRepository<Tag, Long> {
+    List<Tag> findByTitle(@Param("name") String name);
+}
