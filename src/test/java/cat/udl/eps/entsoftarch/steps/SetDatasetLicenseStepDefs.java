a3f3587559b28f5b3650451d67dc1779601fc856
victorserrate <sarte_789@hotmail.com>
2017-03-13 20:21:34 +0100

    Set closed license implemented, null error

--- src/test/java/cat/udl/eps/entsoftarch/steps/SetDatasetLicenseStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/steps/SetDatasetLicenseStepDefs.java
@@ -1,10 +1,12 @@
 package cat.udl.eps.entsoftarch.steps;
 
+import cat.udl.eps.entsoftarch.domain.ClosedLicense;
 import cat.udl.eps.entsoftarch.domain.Dataset;
 import cat.udl.eps.entsoftarch.domain.OpenLicense;
 import cat.udl.eps.entsoftarch.repository.ClosedLicenseRepository;
 import cat.udl.eps.entsoftarch.repository.DatasetRepository;
 import cat.udl.eps.entsoftarch.repository.OpenLicenseRepository;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
@@ -25,6 +27,9 @@ public class SetDatasetLicenseStepDefs {
     @Autowired
     private DatasetRepository datasetRepository;
 
+
+    /*OPEN LICENSE*/
+
     @When("^I set the open license with text \"([^\"]*)\" to dataset with title \"([^\"]*)\"$")
     public void setOpenLicenseToDataset(String text, String title) throws Throwable {
         OpenLicense openLicense = openLicenseRepository.findByText(text).get(0);
@@ -37,5 +42,20 @@ public class SetDatasetLicenseStepDefs {
         Dataset dataset = datasetRepository.findByTitle(title).get(0);
         OpenLicense openLicense = dataset.getOpenLicense();
         assertEquals(openLicense.getText(),text);
+    }
+
+    /*CLOSED LICENSE*/
+    @When("^I set the closed license with text \"([^\"]*)\" and price (\\d+) to dataset with title \"([^\"]*)\"$")
+    public void iSetTheClosedLicenseWithTextAndPriceToDatasetWithTitle(String text, int count, String title) throws Throwable {
+        ClosedLicense closedLicense = closedLicenseRepository.findByText(text).get(0);
+        Dataset dataset = datasetRepository.findByTitle(title).get(0);
+        dataset.setClosedLicense(closedLicense);
+    }
+
+    @Then("^The dataset with title \"([^\"]*)\" has a closed license with text \"([^\"]*)\" and price (\\d+)$")
+    public void theDatasetWithTitleHasAClosedLicenseWithTextAndPrice(String title, String text, int count) throws Throwable {
+        Dataset dataset = datasetRepository.findByTitle(title).get(0);
+        ClosedLicense closedLicense = dataset.getClosedLicense();
+        assertEquals(closedLicense.getText(),text);
     }
 }
--- src/test/resources/features/SetDatasetLicense.feature
+++ src/test/resources/features/SetDatasetLicense.feature
@@ -19,10 +19,10 @@ Feature: Set Dataset License
     And There are 0 closed licenses registered
     And There are 0 datasets registered
     When I register a closed license with text "My first closed license" and price 10
-    Then The new closed license has text "My first license" and price 10
+    Then The new closed license has text "My first closed license" and price 10
     And There is 1 closed license registered
     When I register a dataset with title "My dataset"
     Then The new dataset has title "My dataset"
     And There is 1 dataset registered
-    When I set the closed license with text "My first license" and price 10 to dataset with title "My dataset"
+    When I set the closed license with text "My first closed license" and price 10 to dataset with title "My dataset"
     Then The dataset with title "My dataset" has a closed license with text "My first closed license" and price 10
