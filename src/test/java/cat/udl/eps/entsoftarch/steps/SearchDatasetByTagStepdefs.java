00e0fedac64ae6d430727a629c793221096fbb86
loc3 <hykrenx@gmail.com>
2017-03-14 16:46:14 +0100

    - Added "@When" at SearchDatasetByTagStepDefs(Issue #13)

---
+++ src/test/java/cat/udl/eps/entsoftarch/steps/SearchDatasetByTagStepdefs.java
@@ -0,0 +1,24 @@
+package cat.udl.eps.entsoftarch.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+/**
+ * Created by loc3 on 14/03/17.
+ */
+public class SearchDatasetByTagStepdefs {
+
+    @Autowired
+    private StepDefs stepDefs;
+
+    @When("^I search dataset with tag \"([^\"]*)\"$")
+    public void iSearchDatasetWithTag(String tagName) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/datasets/search/findByTaggedWith_Name?tag={tag}",tagName))
+                .andDo(print());
+    }
+}
