4d15b25c658effb0b36bfaa54e4222e3dd398f55
Santi Gatius <sgv1@alumnes.udl.cat>
2017-03-20 17:26:51 +0100

    Avanç

--- src/test/java/cat/udl/eps/entsoftarch/steps/CommentAboutDataSetStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/steps/CommentAboutDataSetStepDefs.java
@@ -19,11 +19,15 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 public class CommentAboutDataSetStepDefs {
@@ -35,6 +39,8 @@ public class CommentAboutDataSetStepDefs {
     private DataOwnerRepository dataOwnerRepository;
     @Autowired
     private DatasetRepository datasetRepository;
+    @Autowired
+    private CommentRepository commentRepository;
     @Autowired
     private StepDefs stepDefs;
     private Dataset dataset;
@@ -48,8 +54,12 @@ public class CommentAboutDataSetStepDefs {
         // Clear authentication credentials at the start of every test.
         CommentAboutDataSetStepDefs.currentPassword = "";
         CommentAboutDataSetStepDefs.currentUsername = "";
-        this.dataset = new Dataset();
         zonedDateTime = ZonedDateTime.now();
+        this.dataset = new Dataset();
+        dataset.setTitle("dataset");
+        dataset.setOwner(dataOwnerRepository.findOne(currentUsername));
+        dataset.setDateTime(zonedDateTime);
+        datasetRepository.save(dataset);
     }
 
 
@@ -59,25 +69,24 @@ public class CommentAboutDataSetStepDefs {
         Dataset data = datasetRepository.findByTitle(dataset).get(0);
         User userC = dataOwnerRepository.findOne(user);
         Comment comment = new Comment();
+        comment.setUser(userC);
+        comment.setAbout(data);
         comment.setTxt(text);
         comment.setDateTime(zonedDateTime);
         String message = stepDefs.mapper.writeValueAsString(comment);
-        stepDefs.result = stepDefs.mockMvc.perform(
-                post("/comments")
+        stepDefs.result = stepDefs.mockMvc.perform( post("/comments")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(message)
                         .accept(MediaType.APPLICATION_JSON)
                         .with(authenticate()))
-                .andDo(print());
+                        .andDo(print());
     }
 
-    @Then("^This dataset has a new comment$")
-    public void thisDatasetHasANewComment() throws Throwable {
+    @Then("^This comment comment about dataset \"([^\"]*)\"$")
+    public void thisCommentCommentAboutDataset(String arg0) throws Throwable {
+        Dataset dataset = datasetRepository.findByTitle(arg0).get(0);
+        Comment comment = commentRepository.
         // Write code here that turns the phrase above into concrete actions
-        /*Comment comment = new Comment();
-        comment.setTxt(text);
-        comment.setUser(user);
-        comment.setDateTime(zonedDateTime);*/
         throw new PendingException();
     }
 
