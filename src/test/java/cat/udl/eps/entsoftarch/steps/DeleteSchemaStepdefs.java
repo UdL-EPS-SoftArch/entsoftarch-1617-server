2feb3d566e991f732076aaedef3595264cffe225
yatan <yatan87@gmail.com>
2017-03-27 15:59:01 +0200

    Delete schema with fields

--- src/test/java/cat/udl/eps/entsoftarch/steps/DeleteSchemaStepdefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/steps/DeleteSchemaStepdefs.java
@@ -1,7 +1,10 @@
 package cat.udl.eps.entsoftarch.steps;
 
+import cat.udl.eps.entsoftarch.domain.Field;
 import cat.udl.eps.entsoftarch.domain.Schema;
+import cat.udl.eps.entsoftarch.repository.FieldRepository;
 import cat.udl.eps.entsoftarch.repository.SchemaRepository;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,6 +19,7 @@ public class DeleteSchemaStepdefs {
 
     @Autowired StepDefs stepDefs;
     @Autowired SchemaRepository schemaRepository;
+    @Autowired FieldRepository fieldRepository;
 
 
     @When("^I delete the schema with title \"([^\"]*)\"$")
@@ -23,6 +27,15 @@ public class DeleteSchemaStepdefs {
         Schema schema = schemaRepository.findByTitle(title).get(0);
         stepDefs.result = stepDefs.mockMvc.perform(
                 delete("/schemas/{id}", schema.getId())
+                        .with(authenticate()))
+                .andDo(print());
+    }
+
+    @When("^I delete the field with title \"([^\"]*)\"$")
+    public void iDeleteTheFieldWithTitle(String title) throws Throwable {
+        Field field = fieldRepository.findByTitle(title).get(0);
+        stepDefs.result = stepDefs.mockMvc.perform(
+                delete("/fields/{id}", field.getId())
                         .with(authenticate()))
                 .andDo(print());
     }
