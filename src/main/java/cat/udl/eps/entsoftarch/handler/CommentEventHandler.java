7b5686f5df6dce4958909d6e06434f41c13d5ed3
Chaimae El Morabet Lachkar <elmchai401@gmail.com>
2017-03-30 16:09:36 +0200

    Changed @PreAuthorize at CommentEventHandler

--- src/main/java/cat/udl/eps/entsoftarch/handler/CommentEventHandler.java
+++ src/main/java/cat/udl/eps/entsoftarch/handler/CommentEventHandler.java
@@ -1,18 +1,14 @@
 package cat.udl.eps.entsoftarch.handler;
 
 import cat.udl.eps.entsoftarch.domain.Comment;
-import cat.udl.eps.entsoftarch.domain.DataOwner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * Created by Chaimae
- */
+
 
 @Component
 @Transactional
@@ -21,27 +17,26 @@ public class CommentEventHandler {
     private final Logger logger = LoggerFactory.getLogger(CommentEventHandler.class);
 
     @HandleBeforeCreate
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("#comment.about.owner.username == principal.username")
     public void handleCommentPreCreate(Comment comment) {
         logger.info("Before creating: {}", comment.toString());
 
-        DataOwner principal = (DataOwner) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        comment.setUser(principal);
     }
 
     @HandleBeforeSave
-    @PreAuthorize("#comment.user.username == principal.username")
+    @PreAuthorize("#comment.about.owner.username == principal.username")
     public void handleCommentPreSave(Comment comment) {
         logger.info("Before updating: {}", comment.toString());
     }
 
     @HandleBeforeDelete
-    @PreAuthorize("#comment.user.username == principal.username")
+    @PreAuthorize("#comment.about.owner.username == principal.username")
     public void handleCommentPreDelete(Comment comment) {
         logger.info("Before deleting: {}", comment.toString());
     }
 
     @HandleBeforeLinkSave
+    @PreAuthorize("#comment.about.owner.username == principal.username")
     public void handleCommentPreLinkSave(Comment comment, Object o) {
         logger.info("Before linking: {} to {}", comment.toString(), o.toString());
     }
