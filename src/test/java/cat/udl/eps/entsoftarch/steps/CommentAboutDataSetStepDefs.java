215b9bf7d15dbd4d91f30dbf564c859e1fce503f
Chaimae El Morabet Lachkar <elmchai401@gmail.com>
2017-03-31 16:19:55 +0200

    Fixed error at StepDefs. All tests OK

--- src/test/java/cat/udl/eps/entsoftarch/steps/CommentAboutDataSetStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/steps/CommentAboutDataSetStepDefs.java
@@ -2,10 +2,8 @@ package cat.udl.eps.entsoftarch.steps;
 
 import cat.udl.eps.entsoftarch.domain.Comment;
 import cat.udl.eps.entsoftarch.domain.Dataset;
-import cat.udl.eps.entsoftarch.domain.User;
-import cat.udl.eps.entsoftarch.repository.CommentRepository;
 import cat.udl.eps.entsoftarch.repository.DatasetRepository;
-import cat.udl.eps.entsoftarch.repository.UserRepository;
+import com.jayway.jsonpath.JsonPath;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -24,26 +22,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class CommentAboutDataSetStepDefs {
 
-    private static ZonedDateTime zonedDateTime;
-    @Autowired
-    private UserRepository dataOwnerRepository;
     @Autowired
     private DatasetRepository datasetRepository;
-    @Autowired
-    private CommentRepository commentRepository;
     @Autowired
     private StepDefs stepDefs;
 
 
-
-    @When("^I comment a dataset \"([^\"]*)\" with text \"([^\"]*)\" and user \"([^\"]*)\"$")
-    public void iCommentADataset(String dataset, String text, String user) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    @When("^I comment a dataset \"([^\"]*)\" with text \"([^\"]*)\"$")
+    public void iCommentADataset(String dataset, String text) throws Throwable {
         Dataset data = datasetRepository.findByTitle(dataset).get(0);
-        User userC = dataOwnerRepository.findOne(user);
         Comment comment = new Comment();
         comment.setText(text);
-        comment.setDateTime(zonedDateTime);
         comment.setAbout(data);
         String message = stepDefs.mapper.writeValueAsString(comment);
 
@@ -54,46 +43,26 @@ public class CommentAboutDataSetStepDefs {
                 .accept(MediaType.APPLICATION_JSON)
                 .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
-
-        commentRepository.save(comment);
-
 
     }
 
     @Then("^The new comment with text \"([^\"]*)\" has been published with username \"([^\"]*)\"$")
     public void theNewCommentWithTextHasBeenPublishedWithUsername(String text, String user) throws Throwable {
         stepDefs.result.andExpect(jsonPath("$.text", is(text)));
-        stepDefs.result.andExpect(jsonPath("$.user", is(user)));
+
+        String response = stepDefs.result.andReturn().getResponse().getContentAsString();
+        String userComment = (JsonPath.read(response, "$._links.user.href"));
+        String[] parts = userComment.split("/");
+        userComment = parts[5];
+        Assert.assertThat(userComment, is(user));
+
 
     }
 
     @And("^The new comment has date and time approximately now$")
     public void theNewCommentHasDateAndTimeApproximatelyNow() throws Throwable {
-        Comment comment = commentRepository.findOne(0L);
-        Assert.assertThat(comment.getDateTime(), lessThan(ZonedDateTime.now()));
+        String response = stepDefs.result.andReturn().getResponse().getContentAsString();
+        ZonedDateTime dateTime = ZonedDateTime.parse(JsonPath.read(response, "$.dateTime"));
+        Assert.assertThat(dateTime, lessThan(ZonedDateTime.now()));
     }
-
-
-    @When("^I comment a dataset \"([^\"]*)\" with text \"([^\"]*)\"$")
-    public void iCommentADatasetWithText(String arg0, String arg1) throws Throwable {
-        Dataset data = datasetRepository.findByTitle(arg0).get(0);
-        Comment comment = new Comment();
-        comment.setAbout(data);
-        comment.setText(arg1);
-        comment.setDateTime(zonedDateTime);
-
-        String message = stepDefs.mapper.writeValueAsString(comment);
-
-        stepDefs.result = stepDefs.mockMvc.perform(post("/comments")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(message)
-                .accept(MediaType.APPLICATION_JSON)
-                .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print());
-
-
-        commentRepository.save(comment);
-    }
-
-
 }
--- src/test/resources/features/CommentAboutDataSet.feature
+++ src/test/resources/features/CommentAboutDataSet.feature
@@ -4,10 +4,10 @@ Feature: Comment About Dataset
   I want to publish a comment about a Dataset
 
   Scenario: Publish a comment about Dataset with a registered user.
-    Given I login as "owner" with password "password"
+    Given I login as "user" with password "password"
     And There is a dataset with title "dataset" and owner "owner"
-    When I comment a dataset "dataset" with text "text" and user "owner"
-    Then The new comment with text "text" has been published with username "owner"
+    When I comment a dataset "dataset" with text "text"
+    Then The new comment with text "text" has been published with username "user"
     And The new comment has date and time approximately now
 
   Scenario: Publish a comment about Dataset without a registered user.
