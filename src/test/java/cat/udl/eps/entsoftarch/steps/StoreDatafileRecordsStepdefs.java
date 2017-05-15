28232ad250299e26056daa0617529985c582e0ad
Jordi Mèlich <jordicanon@gmail.com>
2017-05-15 21:38:58 +0200

    add tests for StoreDatafileRecords

---
+++ src/test/java/cat/udl/eps/entsoftarch/steps/StoreDatafileRecordsStepdefs.java
@@ -0,0 +1,15 @@
+package cat.udl.eps.entsoftarch.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+
+/**
+ * Created by ubuntu on 15/05/17.
+ */
+public class StoreDatafileRecordsStepdefs {
+    @And("^The datafile contains (\\d+) lines$")
+    public void theDatafileContainsLines(int linesCount) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
--- src/test/resources/features/StoreDatafileRecords.feature
+++ src/test/resources/features/StoreDatafileRecords.feature
@@ -10,3 +10,5 @@ Feature: Upload Data File
     Then The dataset contains a file with filename "test.csv"
     And The datafile content contains "test.csv" content
     And The datafile separator is ","
+    And The datafile contains 3 lines
+    And The line 3 contains "3,Gerard,700"
