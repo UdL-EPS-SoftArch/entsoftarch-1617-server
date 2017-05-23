84dd16612b7587311c1664e54c262a68bddcf6a3
Xavi <xaviermoyafarre@gmail.com>
2017-05-23 15:38:11 +0200

    new method to find field by partOf (schema)

--- src/main/java/cat/udl/eps/entsoftarch/repository/FieldRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/repository/FieldRepository.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.entsoftarch.repository;
 
 import cat.udl.eps.entsoftarch.domain.Field;
+import cat.udl.eps.entsoftarch.domain.Schema;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,5 +12,6 @@ import java.util.List;
 @RepositoryRestResource
 public interface FieldRepository extends PagingAndSortingRepository<Field, Long> {
     List<Field> findByTitle(@Param("title") String title);
+    List<Field> findByPartOf(@Param("partOf") Schema partOf);
 
 }
